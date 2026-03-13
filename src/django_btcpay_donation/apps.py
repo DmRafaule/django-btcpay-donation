29b319dbfce33091bafe827ce0add9251918f69e
DmRafaule <ChedrDen@gmail.com>
2026-03-13 18:16:49 +0300

    Fixed an issue with wrong app name

--- src/django_btcpay_donation/apps.py
+++ src/django_btcpay_donation/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BtcpaydonationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BTCPayDonation'
+    name = 'django_btcpay_donation'
